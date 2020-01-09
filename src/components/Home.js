7cd2cfb4f29bf20ac3f637e2ddffd4087a53827b
Amantina Sosa <amantinasosa@gmail.com>
2020-01-09 12:36:42 -0500

    complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,37 +1,50 @@
 import React, { Component } from 'react';
 import Hero from './subcomponents/Hero';
 import BlogThumb from './subcomponents/BlogThumb';
-
+import axios from 'axios';
 // import axios
 
-class Home extends Component{
-    constructor(){
-        super();
-        this.state = {
-            featured: '',
-            index: 0,
-            posts: [{title: "Loading...",image: 'https://unsplash.it/900/400/?random'}]
-        }
-    }
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      featured: '',
+      index: 0,
+      posts: [
+        { title: 'Loading...', image: 'https://unsplash.it/900/400/?random' },
+      ],
+    };
+  }
 
-    // insert componentWillMount:
-    
+  // insert componentWillMount:
+  componentWillMount() {
+    axios
+      .get('api/featured')
+      .then(results => {
+        this.setState({
+          featured: results.data,
+          index: ~~(Math.random() * results.data.length) + 0,
+          posts: results.data,
+        });
+      })
+      .catch(console.log);
+  }
 
-    render(){
-        // map over your recommended blogs here, replace null.
-        const posts = null
+  render() {
+    // map over your recommended blogs here, replace null.
+    const posts = null;
 
-        return(
-            <div className="content" >
-                <Hero blog={this.state.posts[this.state.index]} />
-                <hr/>
-                <div className="blog-grid">
-                    {/* put your mapped blogs here */}
-                    {posts}
-                </div>
-            </div>
-        )
-    }
+    return (
+      <div className="content">
+        <Hero blog={this.state.posts[this.state.index]} />
+        <hr />
+        <div className="blog-grid">
+          {/* put your mapped blogs here */}
+          {posts}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
--- src/components/NewUser.js
+++ src/components/NewUser.js
@@ -1,126 +1,159 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 //import axios
-import axios from 'axios'
 
-class NewUser extends Component{
-    constructor(){
-        super()
-        this.state={
-            name: '',
-            img: '',
-            desc: ''
-        }
+class NewUser extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      img: '',
+      desc: '',
+    };
+  }
+
+  // insert addUser
+  addUser() {
+    axios
+      .post(`/api/users`, this.state)
+      .then(results => {
+        this.props.history.push(`/results.data/${results.id}`);
+      })
+      .catch(console.log);
+  }
+
+  // insert updateUser
+  updateUser() {
+    axios
+      .put(`/api/users`)
+      .then(results => {
+        this.props.history.push(`/results.data/${results.data.id}`);
+      })
+      .catch(console.log);
+  }
+
+  // insert deleteUser
+  deleteUser() {
+    axios
+      .delete(`/api/users`)
+      .then(results => {
+        this.props.history.push(`/search`);
+      })
+      .catch(console.log);
+  }
+
+  render() {
+    return (
+      <div className="content">
+        {this.state.warn ? (
+          <h4 style={{ color: 'red' }}>You must include at least a name.</h4>
+        ) : null}
+        <form className="new-user" onSubmit={e => this.submit(e)}>
+          <div className="pic-input input-group">
+            {this.state.img ? (
+              <img src={this.state.img} alt="profile pic" />
+            ) : (
+              <div className="pic-placeholder">No Image Yet</div>
+            )}
+            <span>
+              <label>Image URL</label>
+              <input
+                type="text"
+                name="img"
+                value={this.state.img}
+                onChange={e => this.handleChange(e)}
+              />
+            </span>
+          </div>
+          <div className="input-group in-group">
+            <label>Name</label>
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={e => this.handleChange(e)}
+            />
+          </div>
+          <div className="input-group in-group">
+            <label>Description</label>
+            <textarea
+              name="desc"
+              value={this.state.desc}
+              onChange={e => this.handleChange(e)}
+            />
+          </div>
+          {this.state.id >= 0 ? (
+            <span>
+              <button className="delete-button">Delete User</button>
+              <button type="submit">Update</button>
+            </span>
+          ) : (
+            <button type="submit">Save</button>
+          )}
+        </form>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    let id = this.props.match.params.id;
+    if (id >= 0) {
+      axios.get(`/api/user/${id}`).then(response => {
+        this.setState({
+          id: id,
+          name: response.data.name,
+          img: response.data.img,
+          desc: response.data.desc,
+        });
+      });
     }
-    
-    // insert addUser
+  }
 
-
-    // insert updateUser    
-
-
-    // insert deleteUser
-
-
-    render(){
-        return(
-            <div className="content">
-                {this.state.warn?<h4 style={{color: 'red'}}>You must include at least a name.</h4>:null}
-                <form className='new-user' onSubmit={e=>this.submit(e)}>
-                    <div className="pic-input input-group">
-                        {
-                            this.state.img
-                            ?
-                            <img src={this.state.img} alt="profile pic"/> 
-                            :
-                            <div className="pic-placeholder">No Image Yet</div>
-                        }
-                        <span>
-                            <label>Image URL</label>
-                            <input type='text' name='img' value={this.state.img} onChange={e=>this.handleChange(e)} />
-                        </span>
-                    </div>
-                    <div className="input-group in-group">
-                        <label>Name</label>
-                        <input type='text' name='name' value={this.state.name} onChange={e=>this.handleChange(e)} />
-                    </div>
-                    <div className="input-group in-group">
-                        <label>Description</label>
-                        <textarea name='desc' value={this.state.desc} onChange={e=>this.handleChange(e)} />
-                    </div>
-                    {
-                        this.state.id >= 0
-                        ?
-                        <span>
-                            <button className='delete-button'>Delete User</button>
-                            <button type='submit'>Update</button>
-                        </span>
-                        :
-                        <button type='submit'>Save</button>
-                    }
-                </form>
-            </div>
-        )
+  componentWillReceiveProps(newProps) {
+    if (newProps.match.params.hasOwnProperty('id')) {
+      let id = newProps.match.params.id;
+      axios.get(`/api/user/${id}`).then(response => {
+        this.setState({
+          id: id,
+          name: response.data.name,
+          img: response.data.img,
+          desc: response.data.desc,
+        });
+      });
+    } else {
+      this.setState({
+        id: null,
+        name: '',
+        img: '',
+        desc: '',
+      });
     }
+  }
 
-    componentDidMount(){
-        let id = this.props.match.params.id
-        if(id >= 0){
-             axios.get(`/api/user/${id}`).then(response=>{
-                 this.setState({
-                     id: id,
-                     name: response.data.name,
-                     img: response.data.img,
-                     desc: response.data.desc
-                 })
-             })
-        }
+  submit(e) {
+    e.preventDefault();
+    if (this.state.name) {
+      if (this.props.match.params.hasOwnProperty('id')) {
+        this.updateUser();
+      } else {
+        this.addUser();
+      }
+    } else {
+      this.setState({ warn: true });
+      setTimeout(() => {
+        this.setState({ warn: false });
+      }, 2000);
     }
-
-    componentWillReceiveProps(newProps){
-        if (newProps.match.params.hasOwnProperty('id')){
-            let id = newProps.match.params.id    
-            axios.get(`/api/user/${id}`).then(response=>{
-                this.setState({
-                    id: id,
-                    name: response.data.name,
-                    img: response.data.img,
-                    desc: response.data.desc
-                })
-            })
-        } else {
-            this.setState({
-                id: null,
-                name: '',
-                img: '',
-                desc: ''
-            })
-        }
-    }
-
-    submit(e){
-        e.preventDefault();
-        if (this.state.name){
-            if (this.props.match.params.hasOwnProperty('id')){
-                this.updateUser()
-            } else {
-                this.addUser()
-            }
-        } else {
-            this.setState({warn: true})
-            setTimeout(()=> {
-                this.setState({warn: false})
-            }, 2000);
-        }
-    }
-    handleChange(e){
-        let { value, name } = e.target
-        this.setState(_=>{
-            let newState={}
-            newState[name]=value
-            return newState
-        })
-    }
+  }
+  handleChange(e) {
+    let { value, name } = e.target;
+    this.setState(_ => {
+      let newState = {};
+      newState[name] = value;
+      return newState;
+    });
+  }
 }
 
-export default NewUser
+export default NewUser;
